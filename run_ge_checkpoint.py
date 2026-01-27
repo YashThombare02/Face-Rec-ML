6a7f3f0cb25a00aa28f970cbf83d959ecd19a875
Yash Thombare <ythombare1972@gmail.com>
2026-01-27 12:02:53 +0530

    Update run_ge_checkpoint.py

--- run_ge_checkpoint.py
+++ run_ge_checkpoint.py
@@ -1,18 +1,36 @@
 import great_expectations as gx
+from great_expectations.checkpoint import SimpleCheckpoint
 
-CHECKPOINT_NAME = "data_checkpoint"
+CHECKPOINT_NAME = "image_checkpoint"
 
 def main():
-    print(" Running Great Expectations checkpoint...")
+    print("Running Great Expectations checkpoint...")
 
+    # Load context (ephemeral context)
     context = gx.get_context()
-    result = context.run_checkpoint(checkpoint_name=CHECKPOINT_NAME)
+
+    checkpoint = SimpleCheckpoint(
+        name=CHECKPOINT_NAME,
+        data_context=context,
+        validations=[
+            {
+                "batch_request": {
+                    "datasource_name": "my_datasource",
+                    "data_connector_name": "default_inferred_data_connector_name",
+                    "data_asset_name": "image_metadata.csv",
+                },
+                "expectation_suite_name": "image_quality_suite",
+            }
+        ],
+    )
+
+    result = checkpoint.run()
 
     if not result["success"]:
-        print(" Data Quality Check Failed!")
-        raise SystemExit(1)
+        raise Exception(" Data quality validation failed")
 
-    print(" Data Quality Check Passed Successfully!")
+    print(" Data quality validation passed")
+
 
 if __name__ == "__main__":
     main()
